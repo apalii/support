a455688feb2d0262fa72877f25b08772a0391696
Andrii Palii <Andrii.Palii@portaone.com>
2015-02-10 14:40:15 +0200

    a lot of fixes+timeleft+wh

--- ticket_watcher.py
+++ ticket_watcher.py
@@ -31,7 +31,13 @@ if pargs.debug:
 
 
 def get_tickets(login, password):
-    '''Getting open tickets via RT API 
+    '''Getting open tickets via RT API. Respond example:
+    RT/3.8.4 200 Ok
+
+    Found 1 tickets
+    Page 1/1
+
+    384190: Number Portability for incoming leg
     '''
     rt_url = 'https://rt.portaone.com/REST/1.0/search/ticket?query='
     request = urllib2.quote("Owner='{owner}' AND Status = 'open'".format(owner=login))
@@ -45,8 +51,9 @@ def get_tickets(login, password):
         if pargs.debug:
             print(rt_url + request)
         my_session.post(rt_url, data=payload)
-        data = my_session.get(rt_url + request)
-    return data
+        data = my_session.get(rt_url + request).text.split('\n\n')[2:]
+        data_list = data[0].rstrip().split('\n')
+    return set(data_list)
 
 
 def vk_message(userid, message):
@@ -63,6 +70,7 @@ def vk_message(userid, message):
 
 def notification(tickets_list):
     '''Docstring 
+    ticket_number = rest_response.text.split('\n\n')[1].split('\n')[0]
     '''
     my_radio = 'http://www.radioroks.ua/player/hardnheavy/'
     webbrowser.get('/usr/bin/google-chrome %s').open_new_tab(my_radio)
@@ -74,41 +82,62 @@ def notification(tickets_list):
         to_send = '\n'.join(i for i in tickets_list)
         vk_message(userid='17222429', message=to_send)
 
+def working_hours():
+    '''days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+    So 0 means monday etc. Hence (0,2,3) means "monday","wednesday","thursday".
+    '''
+    if datetime.date.today().weekday() in (0,2,3):
+        return 19
+    else:
+        return 18
+
+
+def time_left(working_hours):
+    '''Shows how many hours and minutes left:
+    '''
+    dt  = datetime.datetime
+    now = dt.now()
+    left_obj = dt(year=now.year,month=now.month,day=now.day,hour=working_hours) - now
+    left = left_obj.__str__().split(':')[:-1]
+    return '{}h:{}m  left'.format(left[0], left[1])
+
 
 if __name__ == "__main__":
-    #ticket_number = rest_response.text.split('\n\n')[1].split('\n')[0]
     if not len(sys.argv) == 1:
         os.system('clear')
         print('Press ctrl+C to exit')
         watched_list = set()
         iteration = 0
-        while datetime.datetime.now().time() < datetime.time(hour=18, minute=00):
+        wh = working_hours()
+        while datetime.datetime.now().time() < datetime.time(hour=wh, minute=00):
             iteration += 1
-            rest_response = get_tickets(pargs.login, pargs.password)
-            open_tickets = rest_response.text.split('\n\n')[2:]
+            open_tickets = get_tickets(pargs.login, pargs.password)
+            if pargs.debug:
+                print('iteration    : {}'.format(iteration))
+                print('watched_list : {}'.format(watched_list))
+                print('open_tickets : {}'.format(open_tickets))
             if iteration == 1:
                 os.system('notify-send "Ticket watcher started..."')
-                if len(open_tickets) >= 1 and 'No matching results.' \
-                                            not in open_tickets[0]: 
+                if len(open_tickets) >= 1:
+                    os.system('notify-send "Open tickets:"')
                     for ticket in open_tickets:
                         watched_list.add(ticket)
-                        os.system('notify-send "Open tickets:"')
                         os.system('notify-send "{}"'.format(ticket))
                     time.sleep(300)
                 else:
                     os.system('notify-send "There are no tickets !"')
-                    os.system('notify-send "You are lucky (: "')
                     time.sleep(300)
             else:
-                if len(open_tickets) == len(watched_list) or len(watched_list) == 0:
+                if len(open_tickets.difference(watched_list)) == 0:
                     os.system('notify-send "No new tickets"')
+                    os.system('notify-send "{}"'.format(time_left(wh)))
                     print(time.ctime(time.time()) + ' no new tickets so far')
                     time.sleep(300)
                 else:
-                    notification(open_tickets)
-                    for ticket in open_tickets:
+                    tickets_dif = open_tickets.difference(watched_list)
+                    notification(tickets_dif)
+                    for ticket in tickets_dif:
                         watched_list.add(ticket)
-                    time.sleep(20)
+                    time.sleep(300)
     else:
         print('Use --help for more detail')
-
